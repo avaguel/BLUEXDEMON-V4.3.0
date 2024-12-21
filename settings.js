aa3f60c75cb309f846852a162105db739299217d
INSANEFF <154343572+INSANEFF@users.noreply.github.com>
2024-12-21 19:47:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.alwaysonline = true
 
 global.autoswview = false
 
-global.public = true
+global.public = false 
 
 global.autoreact = false
 
@@ -20,7 +20,7 @@ global.antidelete = false
 
 global.autotyping = false
 
-global.autoBio = true
+global.autoBio = false 
 
 global.antiSpam = true
 
@@ -28,9 +28,9 @@ global.sign = "〆"
 // ☞ ➻ ➽ ☛ 〆 
 global.bank = "Opay"
 
-global.bankname = "BLUE"
+global.bankname = "NOSA"
 
-global.accnumber = "7041039367"
+global.accnumber = "9112184365"
 
 global.language = "en"
 global.sessionName = "session"
